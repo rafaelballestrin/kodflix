bb673e313d09ce85e3fdaa38f0fa0b435cc461d6
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-01-10 15:44:20 +0000

    #8 Completed task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import Details from './Details.js';
 import Gallery from './Gallery.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <Gallery />
+        <Route exact path='/' component={Gallery} />
+        <Route exact path='/details' component={Details} />
       </div>
+      </Router>
     );
   }
 }
-
 
 export default App;
---
+++ src/Details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Details() {
+    return (
+      <div>
+        <h1>Details page Test</h1>
+      <Link to='/'>Back to Home Page</Link>
+      </div>
+    );
+  }
