ac192fe6fa0abf86a29f725da9ed2f9ffc5cb197
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-01-21 13:57:42 +0000

    #11 Improve the gallery layout

--- src/Gallery.js
+++ src/Gallery.js
@@ -9,18 +9,16 @@ import coverTheWalkingDead from './images/coverTheWalkingDead.jpg'
 import coverTheWire from './images/coverTheWire.jpg'
 
 export default function Gallery() {
-    return (
-      <div>
-        <div className="container">
-          <Stack id='BlackMirror' name='Black Mirror' logo={coverBlackMirror} />
-          <Stack id='BreakingBad' name='Breaking Bad' logo={coverBreakingBad} />
-          <Stack id='DeathNote' name='Death Note' logo={coverDeathNote} />
-        </div>
-        <div className="container">
-          <Stack id='GameOfThrones' name='Game of Thrones' logo={coverGameOfThrones} />
-          <Stack id='TheWalkingDead' name='The Walking Dead' logo={coverTheWalkingDead} />
-          <Stack id='TheWire' name='The Wire' logo={coverTheWire} />
-        </div>
-      </div >
-    )
-  }
+  return (
+    <div>
+      <div className="container">
+        <Stack id='BlackMirror' name='Black Mirror' logo={coverBlackMirror} />
+        <Stack id='BreakingBad' name='Breaking Bad' logo={coverBreakingBad} />
+        <Stack id='DeathNote' name='Death Note' logo={coverDeathNote} />
+        <Stack id='GameOfThrones' name='Game of Thrones' logo={coverGameOfThrones} />
+        <Stack id='TheWalkingDead' name='The Walking Dead' logo={coverTheWalkingDead} />
+        <Stack id='TheWire' name='The Wire' logo={coverTheWire} />
+      </div>
+    </div >
+  )
+}
