bec5c4fc49da070e96bf955589938618120e664c
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-01-23 09:30:51 +0000

    #12 Introduce component state - QA Required

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,31 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
+export default class Details extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      welcomeMessage: 'Welcome to the details page, WIP'
+    };
+  }
+
+  componentDiMount() {
+    setTimeout(() => {
+      this.setState({
+        welcomeMessage: 'The best is yet to come!'
+      });
+    }, 3000);
+  }
+
+  render() {
     return (
       <div>
-        <h1>Details page Test</h1>
-      <Link to='/'>Back to Home Page</Link>
+        <h1>{this.state.welcomeMessage}</h1>
+        <Link to='/'>Back to home page</Link>
       </div>
     );
   }
+
+}
