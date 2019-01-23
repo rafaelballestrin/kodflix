71a219978e8cbb73856f34cea0df257f1e945ac3
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-01-23 16:32:13 +0000

    #15 Redirect to a "not-found" page when the targeted show doesn't exist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Details from './Details';
 import Gallery from './Gallery';
+import NotFound from './NotFound'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <Route exact path='/' component={Gallery} />
-        <Route exact path='/:galleryId' component={Details}  />
-      </div>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={Gallery} />
+            <Route exact path='/not-found' component={NotFound} />
+            <Route exact path='/:galleryId' component={Details} />
+          </Switch>
+        </div>
       </Router>
     );
   }
--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import getGallery from './Gallery-get';
 
 export default class Details extends Component {
@@ -16,16 +16,20 @@ export default class Details extends Component {
     let galleryId = this.props.match.params.galleryId;
     let gallery = getGallery()
       .find((gallery) => gallery.id === galleryId);
-      this.setState({ gallery });
+    this.setState({ gallery });
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.state.gallery.name}</h1>
-        <Link to='/'>Back to home page</Link>
-      </div>
-    );
+    if (this.state.gallery === undefined) {
+      return <Redirect to='/not-found' />
+    } else {
+      return (
+        <div>
+          <h1>{this.state.gallery.name}</h1>
+          <Link to='/'>Back to home page</Link>
+        </div>
+      );
+    }
   }
 
 }
