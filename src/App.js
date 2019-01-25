db7e990a876581e10e291695d32f24006312c146
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-01-25 16:04:31 +0000

    #16 Add synopsis & cover to the details page of each TV show - Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Details from './Details';
+import Details from './Details/Details';
 import Gallery from './Gallery';
 import NotFound from './NotFound'
 import './App.css';
--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,7 +1,8 @@
 
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import getGallery from './Gallery-get';
+import getGallery from '../Gallery-get';
+import './Details.css'
 
 export default class Details extends Component {
 
@@ -24,12 +25,18 @@ export default class Details extends Component {
       return <Redirect to='/not-found' />
     } else {
       return (
-        <div>
-          <h1>{this.state.gallery.name}</h1>
-          <Link to='/'>Back to home page</Link>
-        </div>
-      );
-    }
-  }
-
+        <div className='Details'>
+         <h1>{this.state.gallery.name}</h1>
+         <div className ='container'>
+           <div className='synopsis'>{this.state.gallery.synopsis}</div>
+           <img
+             src={this.state.gallery.logo}
+             alt={this.state.gallery.name} />
+         </div>
+         <Link to='/'>Back to home page</Link>
+       </div >
+          );
+        }
+      }
+    
 }
