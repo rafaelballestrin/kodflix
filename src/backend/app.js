4d655ceffbc9ab59ebd9f73be15b656463b9470b
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-02-05 12:19:17 +0000

    Changes in PackageJson

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001;
 const movies = require('./movies');
+const path = require('path');
 
 app.get('/rest/movies', (req, res) => res.send(movies()));
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
